3e9b032f2c39814e3b4ac3ab8f31a4e5b8e5328f
akrnv25 <akrnv25@gmail.com>
2023-03-24 04:41:41 +0400

    logic part

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
 import './styles.css';
+
+const searchInput = document.querySelector('.search__input');
+const searchButton = document.querySelector('.search__button');
+const reloadButton = document.querySelector('.reload-button');
+
+const url = new URL(window.location.href);
+const currentId = url.searchParams.get('id');
+console.log(currentId);
+
+let searchId;
+searchInput.oninput = () => {
+  searchId = searchInput.value;
+}
+searchButton.onclick = () => {
+  console.log(searchId);
+}
+
+reloadButton.onclick = () => {
+  window.location.reload();
+}
+
